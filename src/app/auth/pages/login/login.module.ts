95dce66fa23b46db991c860b4cdb7da13d5158de
Robson <robsonlira_pe@hotmail.com>
2019-07-29 18:44:22 -0300

    Criação dos modulos Shared e Core

--- src/app/auth/pages/login/login.module.ts
+++ src/app/auth/pages/login/login.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [
   {
@@ -16,9 +14,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ],
   declarations: [LoginPage]
